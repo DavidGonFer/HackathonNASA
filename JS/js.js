a32a54757eee6b401d269853733c3ecc5ad9ae51
Maroru24 <64960701+Maroru24@users.noreply.github.com>
2022-10-02 09:30:45 +0100

    casicasicasi

--- JS/js.js
+++ JS/js.js
@@ -5,26 +5,41 @@ let respuestas = ["James Webb", "Hubble", "Spitzer"];
 let respuesta ="";
 function habilitarBoton(){
     document.getElementById("next").style.visibility="visible";    
+let imagenes = ["../../IMG/1aPregunta.jpg","../../IMG/2aPregunta.webp","../../IMG/3aPregunta.jpg"];
+function mostrar(element){
+    document.getElementById(element).style.visibility="visible";    
 }
 
 function checkear(){
     if(respuestas[numPregunta]==respuesta) numAcertadas++;
     numPregunta++;
+}
     if(numPregunta>=respuestas.length) mostrarAcertadas();
+    if(numPregunta>=respuestas.length) {
+        ocultar("esconderResultado");
+        mostrar("resultado");
+        mostrar("restart");
+        mostrarAcertadas();
+        
+    }
+    else {
+        cambiarImagen();
+        ocultar("next");
+    }
 }
 
 function setRespuesta1(){
-    habilitarBoton();
+    mostrar("next");
     respuesta = "James Webb";
 }
 
 function setRespuesta2(){
-    habilitarBoton();
+    mostrar("next");
     respuesta = "Hubble";
 }
 
 function setRespuesta3(){
-    habilitarBoton();
+    mostrar("next");
     respuesta = "Spitzer";
 }
 
@@ -32,4 +47,8 @@ function mostrarAcertadas(){
     document.getElementById("resultado").innerHTML = numAcertadas;
 }
 
+
+function ocultar(elemento){
+    document.getElementById(elemento).style.visibility="hidden";
+}
 
