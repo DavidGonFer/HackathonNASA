6a6665193e66d0890193e2bd98517b12b1a3e8e1
Maroru24 <64960701+Maroru24@users.noreply.github.com>
2022-10-02 10:31:28 +0100

    jsBueno

--- JS/js.js
+++ JS/js.js
@@ -20,22 +20,34 @@ function checkear(){
     }
     else {
         cambiarImagen();
+        quitarSeleccionado("hubble");
+        quitarSeleccionado("spitzer");
+        quitarSeleccionado("jamesWebb");
         ocultar("next");
     }
 }
 
 function setRespuesta1(){
     mostrar("next");
+    mostrarSeleccionado("jamesWebb");
+    quitarSeleccionado("hubble");
+    quitarSeleccionado("spitzer");
     respuesta = "James Webb";
 }
 
 function setRespuesta2(){
     mostrar("next");
+    mostrarSeleccionado("hubble");
+    quitarSeleccionado("jamesWebb");
+    quitarSeleccionado("spitzer");
     respuesta = "Hubble";
 }
 
 function setRespuesta3(){
     mostrar("next");
+    mostrarSeleccionado("spitzer");
+    quitarSeleccionado("jamesWebb");
+    quitarSeleccionado("hubble");
     respuesta = "Spitzer";
 }
 
@@ -51,3 +63,10 @@ function ocultar(elemento){
     document.getElementById(elemento).style.visibility="hidden";
 }
 
+function mostrarSeleccionado(element){
+    document.getElementById(element).style.backgroundColor="rgba(55, 95, 215, 1)";  
+}
+
+function quitarSeleccionado(element){
+    document.getElementById(element).style.backgroundColor="rgba(65, 105, 225, 0.7)";     
+}
